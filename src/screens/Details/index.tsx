89d9262e0efc304f71338c126ef78a292fb7746c
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-01-18 15:11:06 -0300

    feat: Add index.tsx of screen Details

---
+++ src/screens/Details/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import {Text} from 'react-native';
+import { Container, Image } from './styles';
+
+const PlanetDetails: React.FC = () => {
+  return (
+    <Image>
+      <Container>
+        <Text>Hello world</Text>
+      </Container>
+    </Image>
+  );
+};
+
+export default PlanetDetails;
