d9bfdd6da9b98ad4295b6c2b124794028c449963
gavslima <gabrielvitor.limamatias4@gmail.com>
2022-01-19 11:26:22 -0300

    refactoe: Delete comment

--- App.tsx
+++ App.tsx
@@ -3,8 +3,6 @@ import Routes from './src/routes';
 import store from './src/store';
 import { Provider } from 'react-redux';
 
-// import { Container } from './styles';
-
 const App: React.FC = () => {
   return(
     <Provider store={store}>
